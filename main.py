b1e08ef6f7b8613d81a1378b2ed9d0762056b549
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-21 13:25:10 -0400

    Created a pet-friendly column.

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ hotels = pd.read_csv("hotels.csv", index_col=0)
 # Remove the whitespaces in column headers
 hotels.columns = hotels.columns.str.strip()
 
-# Get the Hotel Star Rating & Facilities
-rating_facilities = hotels[["HotelRating", "HotelFacilities"]]
+# Create Pet-Friendly column to indicate if hotel allows pets
+hotels["Pet-Friendly"] = (
+    hotels["HotelFacilities"].str.contains('Pets allowed', na=False) &
+    ~hotels["HotelFacilities"].str.contains('NO Large pets allowed|NO Small pets allowed', na=False)
+)
 
-print(rating_facilities.head())
+# Output only the Hotel Star Rating & Pet-Friendly columns
+rating_pets = hotels[["HotelRating", "Pet-Friendly"]]
+
+print(rating_pets.head())
