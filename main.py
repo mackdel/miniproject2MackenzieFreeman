c4e346b7b47c1afd5e299fc249f9e216d38dd302
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-21 18:41:56 -0400

    Created pie chart of portion of pet-friendly and non-pet-friendly hotels.

--- main.py
+++ main.py
@@ -29,10 +29,16 @@ hotels_filtered.loc[:, 'HotelRating'] = hotels_filtered['HotelRating'].replace({
     'OneStar': '1'
 })
 
-# Bar Chart: Amount of Pet-Friendly Hotels by Star Rating
-pet_friendly_counts = hotels_filtered[hotels_filtered["Pet-Friendly"]].groupby('HotelRating').size().reset_index(name='Count')
+# 1. Bar Chart: Amount of Pet-Friendly Hotels by Star Rating
+pet_friendly_counts = hotels_filtered[hotels_filtered["Pet-Friendly"]].groupby('HotelRating').size().reset_index(name='Count') # Count of Pet-Friendly hotels grouped by star rating
 plt.bar(pet_friendly_counts['HotelRating'], pet_friendly_counts['Count'], color='r')
 plt.title("Number of Pet-Friendly Hotels by Star Rating")
 plt.xlabel("Star Rating")
 plt.ylabel("Number of Pet-Friendly Hotels")
+plt.show()
+
+# 2. Pie Chart: Portion of Pet-Friendly and Non-Pet-Friendly Hotels
+pet_friendly_counts = hotels_filtered['Pet-Friendly'].value_counts() # Count of both True and False values in Pet-Friendly
+plt.pie(pet_friendly_counts, labels=['Non-Pet-Friendly', 'Pet-Friendly'], autopct='%1.2f%%', colors=['limegreen', 'turquoise'])
+plt.title("Proportion of Pet-Friendly vs Non-Pet-Friendly Hotels")
 plt.show()
