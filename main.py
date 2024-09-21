84e225e1d9cb6b3a67497a0f00e8731acac91fe7
gitmackfree <freemanmackenzie06@gmail.com>
2024-09-21 12:32:51 -0400

    Grabbed hotel star rating and facilities columns.

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ import matplotlib.pyplot as plt
 # Grab the hotel data
 hotels = pd.read_csv("hotels.csv", index_col=0)
 
+# Remove the whitespaces in column headers
+hotels.columns = hotels.columns.str.strip()
+
+# Get the Hotel Star Rating & Facilities
+rating_facilities = hotels[["HotelRating", "HotelFacilities"]]
+
+print(rating_facilities.head())
